1af382cd7b2ef86bb25cb9d6da4b5cda559485f3
Keith <754781293@qq.com>
2024-08-09 16:58:05 +0800

    提交

--- src/view/tailwindcss_test/tailwindcss_test.vue
+++ src/view/tailwindcss_test/tailwindcss_test.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mf-text-3xl mf-font-bold">测试</div>
+  <div class="mf-text-3xl mf-font-bold mf-text-mfcor-800 mf-bg-mfcor-950" >测试</div>
 </template>
 
 <script setup></script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   prefix: 'mf-',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mfcor: {
+          50: '#feffe7',
+          100: '#fbffc1',
+          200: '#fbff86',
+          300: '#fffd41',
+          400: '#fff00d',
+          500: '#fee100',
+          600: '#d1a700',
+          700: '#a67802',
+          800: '#895d0a',
+          900: '#744c0f',
+          950: '#442804',
+        },
+      },
+    },
   },
   plugins: [],
 }
