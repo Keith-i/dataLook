bb656141fc6297c96578ac08aa9a77314e3f6916
Keith <754781293@qq.com>
2024-08-09 15:54:39 +0800

    提交

--- src/view/tailwindcss_test/tailwindcss_test.vue
+++ src/view/tailwindcss_test/tailwindcss_test.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="mytest">测试</div>
+  <div class="text-3xl font-bold underline">测试</div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped>
-.mytest {
-  color: red;
-}
-</style>
+<style lang="scss" scoped></style>
